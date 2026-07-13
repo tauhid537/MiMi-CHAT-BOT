2bfef9ce1c4460d098ea8d7f249b76f8c02b5a24
tauhid537 <ariyankjan7777@gmail.com>
2026-07-13 06:07:54 +0600

    Update fork.js

--- Script/commands/fork.js
+++ Script/commands/fork.js
@@ -12,11 +12,11 @@ module.exports.config = {
 module.exports.run = async function({ api, event }) {
     const message = 
         "🌟 আসসালামু আলাইকুম সম্মানিত বট ইউজার 🌟\n\n" +
-        "আপনি যদি তৈরি করতে চান, তাহলে চলে যেতে পারেন আমাদের ইউটিউব চ্যানেলে। " +
-        "সেখানে গিয়ে ভিডিও দেখে আপনি খুব সহজে বট তৈরি করে নিতে পারবেন। " +
-        "তাহলে আর দেরি না করে এখনি চলে যান আমাদের ইউটিউব চ্যানেলে।\n\n" +
-        "➤ YouTube Channel: https://youtube.com/@scb-community\n\n" +
-        "🔗 GitHub Fork Link: https://github.com/shahadat-sahu/SHAHADAT-CHAT-BOT";
+        "আপনি যদি তৈরি করতে না চান, তাহলে চলে যেতে পারেন আমাদের ইউটিউব চ্যানেলে। " +
+        "সেখানে গিয়ে ভিডিও দেখে আপনি খুব সহজে বট তৈরি করে নিতে পারবেন যদিও করবেন না জানি। " +
+        "তাহলে ১৫-২০ বছর দেরি করে এখনি চলে যান আমাদের ইউটিউব চ্যানেলে।\n\n" +
+        "➤ YouTube Channel: এখনও ইউটিউব চ্যানেল খুলি নাই হুদাই 🐸\n\n" +
+        "🔗 GitHub Fork Link: 5000 টাকা নিয়ে বস তাওহীদ এর ইনবক্স এ মেসেজ দাও😍";
 
     return api.sendMessage(message, event.threadID, event.messageID);
 };
